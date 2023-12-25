c4e390b1df167e46c6aa2be3f633aa46b9c960bf
Saumik Dana <dana.spk5@gmail.com>
2023-12-25 01:38:48 -0600

    updates

---
+++ data/more_utils.py
@@ -0,0 +1,259 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import yfinance as yf  
+import matplotlib.dates as mdates
+from datetime import datetime, timedelta
+import pytz
+
+
+np.random.seed(42)
+
+def format_time(t):
+    """Return a formatted time string 'HH:MM:SS
+    based on a numeric time() value"""
+    m, s = divmod(t, 60)
+    h, m = divmod(m, 60)
+    return f'{h:0>2.0f}:{m:0>2.0f}:{s:0>2.0f}'
+
+class MultipleTimeSeriesCV:
+    """Generates tuples of train_idx, test_idx pairs
+    Assumes the MultiIndex contains levels 'symbol' and 'date'
+    purges overlapping outcomes"""
+
+    def __init__(
+        self,
+        n_splits=3,
+        train_period_length=126,
+        test_period_length=21,
+        lookahead=None,
+        date_idx='date',
+        shuffle=False
+    ):
+        self.n_splits = n_splits
+        self.lookahead = lookahead
+        self.test_length = test_period_length
+        self.train_length = train_period_length
+        self.shuffle = shuffle
+        self.date_idx = date_idx
+
+    def split(self, X, y=None, groups=None):
+        unique_dates = X.index.get_level_values(self.date_idx).unique()
+        days = sorted(unique_dates, reverse=True)
+        split_idx = []
+        for i in range(self.n_splits):
+            test_end_idx = i * self.test_length
+            test_start_idx = test_end_idx + self.test_length
+            train_end_idx = test_start_idx + self.lookahead - 1
+            train_start_idx = train_end_idx + self.train_length + self.lookahead - 1
+            split_idx.append([train_start_idx, train_end_idx, test_start_idx, test_end_idx])
+
+        dates = X.reset_index()[[self.date_idx]]
+        for train_start, train_end, test_start, test_end in split_idx:
+            train_idx = dates[(dates[self.date_idx] > days[train_start])
+                              & (dates[self.date_idx] <= days[train_end])].index
+            test_idx = dates[(dates[self.date_idx] > days[test_start])
+                             & (dates[self.date_idx] <= days[test_end])].index
+            if self.shuffle:
+                np.random.shuffle(list(train_idx))
+            yield train_idx.to_numpy(), test_idx.to_numpy()
+
+    def get_n_splits(self, X, y, groups=None):
+        return self.n_splits
+    
+def get_stock_data(ticker_symbol):
+    # Create a Ticker object
+    stock = yf.Ticker(ticker_symbol)
+
+    # 1. Historical Market Data (for the past 5 days)
+    hist_data = stock.history(period="5d")
+    print(f"Historical Market Data for {ticker_symbol} (Last 5 Days):")
+    print(hist_data)
+
+    # 2. Company Information
+    info = stock.info
+    print(f"\nCompany Information for {ticker_symbol}:")
+    for key, value in info.items():
+        print(f"{key}: {value}")
+
+    # 3. Financial Data
+    # Income Statement
+    income_statement = stock.financials
+    print(f"\nIncome Statement for {ticker_symbol}:")
+    print(income_statement)
+
+    # Balance Sheet
+    balance_sheet = stock.balance_sheet
+    print(f"\nBalance Sheet for {ticker_symbol}:")
+    print(balance_sheet)
+
+    # Cash Flows
+    cash_flow = stock.cashflow
+    print(f"\nCash Flow for {ticker_symbol}:")
+    print(cash_flow)
+
+    # 4. Stock Dividends and Splits
+    dividends = stock.dividends
+    splits = stock.splits
+    print(f"\nDividends for {ticker_symbol}:")
+    print(dividends)
+    print(f"\nStock Splits for {ticker_symbol}:")
+    print(splits)
+    
+    return
+
+def get_options_data(ticker_symbol):
+    # Create a Ticker object
+    stock = yf.Ticker(ticker_symbol)
+
+    # 5. Options Data (if available)
+    try:
+        options_dates = stock.options
+        print(f"\nOptions Expiry Dates for {ticker_symbol}:")
+        print(options_dates)
+        for date in options_dates:
+            options_data = stock.option_chain(date)
+            print(f"\nOptions Data for {ticker_symbol} on {date}:")
+            print("Calls:")
+            print(options_data.calls)
+            print("Puts:")
+            print(options_data.puts)
+    except Exception as e:
+        print(f"\nOptions Data for {ticker_symbol} is not available or an error occurred:", e)
+
+    return
+
+# VIX
+def get_stock_data(ticker, start_date, end_date):
+    """ Fetch historical stock data using yf.Ticker """
+    stock = yf.Ticker(ticker)
+    stock_data = stock.history(start=start_date, end=end_date)
+    return stock_data['Close']
+
+def calculate_beta(stock_data, market_data):
+    """ Calculate the beta of the stock """
+    stock_returns = stock_data.pct_change().dropna()
+    market_returns = market_data.pct_change().dropna()
+
+    covariance = stock_returns.cov(market_returns)
+    variance = market_returns.var()
+
+    return covariance / variance
+
+def plot_stock_vs_market(stock_data, market_data, ticker):
+    """ Plot stock data against market data """
+    normalized_stock = stock_data / stock_data.iloc[0]
+    normalized_market = market_data / market_data.iloc[0]
+
+    # Plotting the closing prices
+    plt.figure(figsize=(3, 3))
+    plt.plot(normalized_stock, label=f'{ticker} Stock Price ')
+    plt.plot(normalized_market, label='S&P 500')
+    plt.title(f'Stock Price vs S&P 500 (Both normalized)', fontsize=8)
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))  # Format as 'Month-Day'
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator())  # Set major ticks to days
+    plt.xticks(rotation=45)  # Rotate for better readability
+    plt.xlabel('Date', fontsize=8)
+    plt.ylabel('Normalized Price', fontsize=8)
+    plt.legend(fontsize=8)
+    plt.grid(True)
+    plt.tick_params(axis='x', labelsize=8)
+
+    plt.show()
+    return
+
+def get_stock_beta(stock_ticker, start_date, end_date):
+    """ Calculate and print the beta of a given stock """
+    market_index_ticker = '^GSPC'  # S&P 500
+
+    stock_data = get_stock_data(stock_ticker, start_date, end_date)
+    market_data = get_stock_data(market_index_ticker, start_date, end_date)
+
+    beta_value = calculate_beta(stock_data, market_data)
+    print(f"Beta of {stock_ticker}: {beta_value}")
+
+    # plot_stock_vs_market(stock_data, market_data, stock_ticker)
+
+    return beta_value
+
+def analyze_stock_performance_post_earnings(ticker, release_date, start_date, end_date):
+    """
+    Analyze the stock performance by comparing the closing price on the day of the earnings release
+    with the closing price on the following day.
+
+    :param ticker: Stock ticker symbol.
+    :param release_day: Number of days from the most recent Monday to the earnings release.
+    :param start_date: Start date for the analysis period.
+    :param end_date: End date for the analysis period.
+    :return: A message indicating whether the stock price increased or decreased after the earnings release.
+    """
+
+    # Fetch the stock data
+    stock_data = get_stock_data(ticker, start_date, end_date)
+
+    release_date = datetime(release_date.year, release_date.month, release_date.day)
+    release_date = pytz.timezone('America/New_York').localize(release_date)
+    
+    # Check if release date data is available
+    if release_date not in stock_data.index:
+        return "Data not available for the release date."
+
+    # Get closing price on the release date
+    release_day_close = stock_data.loc[stock_data.index == release_date].iloc[0]
+    
+    # Calculate the post release date
+    post_release_date = release_date + timedelta(days=1)
+
+    # Check if post release date data is available
+    if post_release_date not in stock_data.index:
+        return "Data not available for the post release date."
+
+    # Get closing price on the post release date
+    post_release_close = stock_data.loc[stock_data.index == post_release_date].iloc[0]
+
+    # Check if the price increased or decreased
+    return "Up" if post_release_close > release_day_close else "Down"
+
+def plot_values_with_directions(values, directions, betas):
+    """
+    Plots a scatter plot of the given values with colors representing the directions and annotates each point with its corresponding beta value.
+    
+    :param values: List of numerical values.
+    :param directions: Corresponding list of directions ('Up' or 'Down').
+    :param betas: List of beta values corresponding to each point.
+    """
+    if len(values) != len(directions) or len(values) != len(betas):
+        raise ValueError("The length of values, directions, and betas lists must be the same.")
+
+    # Convert directions to colors ('Up' as blue and 'Down' as red)
+    colors = ['blue' if d == 'Up' else 'red' for d in directions]
+
+    plt.figure(figsize=(10, 6))
+    sc = plt.scatter(range(len(values)), values, c=colors)
+    
+    # Annotating each point with its corresponding beta value
+    for i, beta in enumerate(betas):
+        plt.annotate(
+            f"{beta:.1f}", (i, values[i]), 
+            textcoords="offset points", 
+            xytext=(0,10), ha='center', fontsize=8
+        )
+
+    plt.title("Scatter Plot of Values with Up/Down Directions and Beta Annotations")
+    plt.xlabel("Index")
+    plt.ylabel("Value")
+    plt.grid(True)
+
+    # Adding a legend for direction
+    plt.scatter([], [], c='blue', label='Up')
+    plt.scatter([], [], c='red', label='Down')
+    plt.legend()
+
+    plt.show()
+
+def get_most_recent_monday():
+    today = datetime.now()
+    # Calculate the number of days to subtract to get the most recent Monday
+    # 0 is Monday, 1 is Tuesday, ... 6 is Sunday
+    days_to_subtract = (today.weekday() - 0) % 7
+    most_recent_monday = today - timedelta(days=days_to_subtract)
+    return most_recent_monday
